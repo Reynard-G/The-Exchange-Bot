0873b25e2bb04d9814df9e7e1ee86a74c0ccc963
Reynard G <40525094+Reynard-G@users.noreply.github.com>
2023-09-14 13:22:46 +0000

    type bug fix

--- slashCommands/subCommands/admin/traders.js
+++ slashCommands/subCommands/admin/traders.js
@@ -15,7 +15,7 @@ module.exports = {
       };
     }));
 
-    const totalBalance = traders.reduce((acc, trader) => acc + trader.balance, 0);
+    const totalBalance = traders.reduce((acc, trader) => acc + Number(trader.balance), 0);
 
     const pages = [];
     for (let i = 0; i < traders.length; i += 5) {
@@ -41,7 +41,7 @@ module.exports = {
         name: "**Total Balance**",
         value: `**${client.utils.formatCurrency(totalBalance)}**`
       });
-      
+
       pages.push(page);
     }
 
