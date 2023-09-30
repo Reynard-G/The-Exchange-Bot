fe8fd006ea87082331316604cb411ac7bb97f7fd
Reynard-G <reynard.ean.gunawan@gmail.com>
2023-09-30 13:15:30 -0500

    manage bigints

--- mysql.js
+++ mysql.js
@@ -8,7 +8,6 @@ const pool = mariadb.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   decimalAsNumber: true,
-  bigIntAsNumber: true,
   connectionLimit: 5
 });
 
@@ -16,6 +15,22 @@ query = async (sql, args) => {
   const conn = await pool.getConnection();
   const result = await conn.query(sql, args);
   conn.release();
+
+  // Convert safe BigInts to Numbers and unsafe BigInts to Strings
+  for (let i = 0; i < result.length; i++) {
+    const row = result[i];
+    for (const key in row) {
+      const value = row[key];
+      if (typeof value === "bigint") {
+        if (value > Number.MAX_SAFE_INTEGER) {
+          row[key] = value.toString();
+        } else {
+          row[key] = Number(value);
+        }
+      }
+    }
+  }
+
   return result;
 };
 
